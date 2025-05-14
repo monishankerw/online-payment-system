80985bce9183ddde88b11d32107dbc176da1eb0d
monishankerw <monishanker121@gmail.com>
2025-05-14 23:01:34 +0530

    user services

---
+++ transactionServices/src/main/java/com/transactionServices/TransactionServicesApplication.java
@@ -0,0 +1,13 @@
+package com.transactionServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionServicesApplication.class, args);
+	}
+
+}
